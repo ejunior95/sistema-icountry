3294f518acb02d95ad5940be5daec391476b8172
ejunior95 <e.junior95@gmail.com>
2021-04-17 00:20:55 -0300

    [UPDATE]-> Preparação incial da estrutura dos arquivos da aplicação e alteração do favicon

---
+++ src/pages/Detalhes/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+function Detalhes() {
+  return(
+      <Container>
+          
+      </Container>
+  );
+}
+
+export default Detalhes;
